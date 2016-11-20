3a45551ef64ef2d2b599ac1cee01c52f39366b8f
Rob <basorot@gmail.com>
2016-11-20 15:44:26 +0200

    MERGED drawroute between clerk location and client location
    Signed-off-by: Rob <basorot@gmail.com>

---
+++ app/src/main/java/com/ohereza/deliveryclerkmobileapp/helper/ParserTask.java
@@ -0,0 +1,91 @@
+package com.ohereza.deliveryclerkmobileapp.helper;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.ohereza.deliveryclerkmobileapp.views.HomeActivity.mMap;
+
+/**
+ * Created by rob on 11/19/16.
+ */
+
+public class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+    // Parsing the data in non-ui thread
+    @Override
+    protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+        JSONObject jObject;
+        List<List<HashMap<String, String>>> routes = null;
+
+        try {
+            jObject = new JSONObject(jsonData[0]);
+            Log.d("ParserTask",jsonData[0].toString());
+            DataParser parser = new DataParser();
+            Log.d("ParserTask", parser.toString());
+
+            // Starts parsing data
+            routes = parser.parse(jObject);
+            Log.d("ParserTask","Executing routes");
+            Log.d("ParserTask",routes.toString());
+
+        } catch (Exception e) {
+            Log.d("ParserTask",e.toString());
+            e.printStackTrace();
+        }
+        return routes;
+    }
+
+    // Executes in UI thread, after the parsing process
+    @Override
+    protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+        ArrayList<LatLng> points;
+        PolylineOptions lineOptions = null;
+
+        // Traversing through all the routes
+        for (int i = 0; i < result.size(); i++) {
+            points = new ArrayList<>();
+            lineOptions = new PolylineOptions();
+
+            // Fetching i-th route
+            List<HashMap<String, String>> path = result.get(i);
+
+            // Fetching all the points in i-th route
+            for (int j = 0; j < path.size(); j++) {
+                HashMap<String, String> point = path.get(j);
+
+                double lat = Double.parseDouble(point.get("lat"));
+                double lng = Double.parseDouble(point.get("lng"));
+                LatLng position = new LatLng(lat, lng);
+
+                points.add(position);
+            }
+
+            // Adding all the points in the route to LineOptions
+            lineOptions.addAll(points);
+            lineOptions.width(5);
+            lineOptions.color(Color.GREEN);
+
+            Log.d("onPostExecute","onPostExecute lineoptions decoded");
+
+        }
+
+        // Drawing polyline in the Google Map for the i-th route
+        if(lineOptions != null) {
+            mMap.addPolyline(lineOptions);
+        }
+        else {
+            Log.d("onPostExecute","without Polylines drawn");
+        }
+    }
+}
